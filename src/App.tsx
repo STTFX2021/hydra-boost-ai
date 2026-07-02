8cb9f233b8fa4526f699716c5c2503c475349ddb
OROCHIKA <sttfx2021@gmail.com>
2026-07-02 10:54:12 +0200

    Register Vozra page route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import Index from "./pages/Index";
+import Vozra from "./pages/Vozra";
 import Servicios from "./pages/Servicios";
 import Precios from "./pages/Precios";
 import Casos from "./pages/Casos";
@@ -28,6 +29,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/vozra" element={<Vozra />} />
             <Route path="/servicios" element={<Servicios />} />
             <Route path="/precios" element={<Precios />} />
             <Route path="/casos" element={<Casos />} />
